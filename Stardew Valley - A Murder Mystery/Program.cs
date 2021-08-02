8c51a79e38460762df17774efe38b42f66006ab2
Drumknott <86430425+Drumknott@users.noreply.github.com>
2021-08-02 18:40:48 +0100

    Add project files.

---
+++ Stardew Valley - A Murder Mystery/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stardew_Valley___A_Murder_Mystery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string PlayerName = Console.ReadLine();
+        }
+    }
+}
